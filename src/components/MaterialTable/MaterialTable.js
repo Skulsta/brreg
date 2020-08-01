7399125b33c07b1eb7c0ceb8cc7d97fcae124475
skulsta <sti.sku@gmail.com>
2020-08-01 23:12:00 +0200

    Dummy data i table

--- src/components/MaterialTable/MaterialTable.js
+++ src/components/MaterialTable/MaterialTable.js
@@ -19,11 +19,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Bedrift AS", "Oslo"),
+  createData("Bedrift AS", "Oslo"),
+  createData("Bedrift AS", "Oslo"),
+  createData("Bedrift AS", "Oslo"),
+  createData("Bedrift AS", "Oslo"),
 ];
 
 export default function SimpleTable() {
@@ -34,8 +34,8 @@ export default function SimpleTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            <TableCell>Navn</TableCell>
+            <TableCell align="right">Sted</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
