ad3e975a1299a7134157cfb5fa7fff5f9a81b6ac
skulsta <sti.sku@gmail.com>
2020-08-01 20:40:02 +0200

    Super minor

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,11 +4,7 @@ import classes from "./Table.module.scss";
 const Table = () => {
   return (
     <div className={classes.Table + " b"}>
-      <p>
-        Suspendisse potenti. Donec accumsan porta felis. Fusce tristique leo
-        quis pede. Cras nibh. Sed eget est vitae tortor mollis ullamcorper.
-        Suspendisse placerat.
-      </p>
+      <p>En nettside helt uten funksjoner.</p>
     </div>
   );
 };
