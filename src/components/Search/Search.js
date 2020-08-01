877ac6023053ba211ffad5f09ebb01f413e283fc
skulsta <sti.sku@gmail.com>
2020-08-01 11:11:03 +0200

    Input field first design and alignment

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,20 +4,10 @@ import classes from "./Search.module.scss";
 
 const Search = () => {
   return (
-    <React.Fragment>
-      <h2 className={classes.header + " b"}>
-        Søk etter bedriftsnavn eller organisasjonsnummer
-      </h2>
-      <label className={classes.search_label} htmlFor="search-input">
-        <input
-          type="text"
-          name="query"
-          id="search-input"
-          placeholder="Bedrift AS"
-        />
-        <i className="fa fa-search search-icon" aria-hidden="true" />
-      </label>
-    </React.Fragment>
+    <form className={classes.Form}>
+      <input type="text" placeholder="Bedrift AS" />
+      <input type="submit" value="Søk" />
+    </form>
   );
 };
 
