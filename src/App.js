d2f9e7d3540ccf5089b04e62cffea8fa123aaf44
skulsta <sti.sku@gmail.com>
2020-08-01 22:48:29 +0200

    Added example table

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.scss";
 
 import Search from "./components/Search/Search";
 import Table from "./components/Table/Table";
+import MaterialTable from "./components/MaterialTable/MaterialTable";
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Search />
-        <Table />
+        <MaterialTable />
       </React.Fragment>
     );
   }
