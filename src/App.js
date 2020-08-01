10f0f07b2a7c32836cd8cc24096585325615ea1c
skulsta <sti.sku@gmail.com>
2020-08-01 20:14:32 +0200

    Added table component with dummy text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import Search from "./components/Search/Search";
+import Table from "./components/Table/Table";
 
 class App extends React.Component {
   render() {
     return (
       <Layout>
         <Search />
+        <Table />
       </Layout>
     );
   }
---
+++ src/components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="b">
+      <p>
+        Suspendisse potenti. Donec accumsan porta felis. Fusce tristique leo
+        quis pede. Cras nibh. Sed eget est vitae tortor mollis ullamcorper.
+        Suspendisse placerat.
+      </p>
+    </div>
+  );
+};
+
+export default Table;
