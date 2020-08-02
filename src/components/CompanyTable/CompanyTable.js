2e38acb910d4a324e8cd2e5a39ea613415114529
skulsta <sti.sku@gmail.com>
2020-08-02 18:58:25 +0200

    Good stuff

--- src/components/CompanyTable/CompanyTable.js
+++ src/components/CompanyTable/CompanyTable.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Table from "react-bootstrap/Table";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Fade from "react-bootstrap/Fade";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import ListGroup from "react-bootstrap/ListGroup";
+import classes from "./CompanyTable.module.scss";
 
 const CompanyTable = (props) => {
   return (
-    <Jumbotron className="bg-white">
+    <Jumbotron className={classes.Table + " bg-white"}>
       <Fade in={props.open} id="example-collapse-text">
         <div className="shadow p-3 mb-5 bg-white rounded">
           <Table>
@@ -34,6 +38,42 @@ const CompanyTable = (props) => {
               </tr>
             </tbody>
           </Table>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <p class="font-weight-bold">Navn</p>
+                </Col>
+                <Col>
+                  <p class="font-weight-bold">Orgnr.</p>
+                </Col>
+                <Col>
+                  <p class="font-weight-bold">Postnr. / Sted</p>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Bedrift AS</Col>
+                <Col>871168172</Col>
+                <Col>0552 Oslo</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Bedrift AS</Col>
+                <Col>871168172</Col>
+                <Col>0552 Oslo</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Bedrift AS</Col>
+                <Col>871168172</Col>
+                <Col>0552 Oslo</Col>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
         </div>
       </Fade>
     </Jumbotron>
