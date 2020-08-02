29606bc9a7e60347923d941c05a8a5c0c70067fa
skulsta <sti.sku@gmail.com>
2020-08-02 16:13:17 +0200

    Table with minimal design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import CompanyTable from "./components/Table/Table";
 
 import "./stylesheets/styles.scss";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Container className="p-3">
-        <Jumbotron>
-          <h1 className="header">Some Header</h1>
-        </Jumbotron>
-      </Container>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Container className="p-3">
+      <Jumbotron className="bg-white">
+        <h1 className="header">Some Header</h1>
+        <CompanyTable>
+          We now have Toasts
+          <span role="img" aria-label="tada">
+            🎉
+          </span>
+        </CompanyTable>
+      </Jumbotron>
+    </Container>
+  );
+};
+
 export default App;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,11 +1,38 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
-const Table = () => {
+const CompanyTable = ({ children }) => {
   return (
-    <div className>
-      <h2>Søk på bedriftsnavn eller organisasjonsnummer</h2>
-    </div>
+    <>
+      <div className="shadow p-3 mb-5 bg-white rounded">
+        <Table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 };
 
-export default Table;
+export default CompanyTable;
