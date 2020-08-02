418ec9d2447be83333e8db92425c88c9ae7acdce
skulsta <sti.sku@gmail.com>
2020-08-02 16:46:53 +0200

    Added input field and button for the table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Fade from "react-bootstrap/Fade";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
 const CompanyTable = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <Button
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        click
-      </Button>
+      <Form>
+        <Form.Row className="align-items-center">
+          <Col sm={2} className="my-1">
+            <Form.Label htmlFor="inlineFormInputName" srOnly>
+              Name
+            </Form.Label>
+            <Form.Control id="inlineFormInputName" placeholder="Jane Doe" />
+          </Col>
+          <Col xs="auto" className="my-1">
+            <Button
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            >
+              click
+            </Button>
+          </Col>
+        </Form.Row>
+      </Form>
+
       <Fade in={open} id="example-collapse-text">
         <div className="shadow p-3 mb-5 bg-white rounded">
           <Table>
