73265a60aacadb11d7e80e079dce2a74f99b67a8
skulsta <sti.sku@gmail.com>
2020-08-02 16:28:11 +0200

    Hide and show table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Fade from "react-bootstrap/Fade";
 
-const CompanyTable = ({ children }) => {
+const CompanyTable = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="shadow p-3 mb-5 bg-white rounded">
-        <Table>
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        click
+      </Button>
+      <Fade in={open} id="example-collapse-text">
+        <div className="shadow p-3 mb-5 bg-white rounded">
+          <Table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </Fade>
     </>
   );
 };
