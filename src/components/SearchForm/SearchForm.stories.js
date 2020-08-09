d07bbee60978e645ea794087c2afd8e97565bbe7
skulsta <sti.sku@gmail.com>
2020-08-09 11:49:49 +0200

    Storybook is set up

---
+++ src/components/SearchForm/SearchForm.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SearchForm from "./SearchForm";
+
+export default {
+  title: "SearchForm",
+  component: SearchForm,
+};
+
+export const Basic = () => <SearchForm />;
+
+// export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+
+// export const Emoji = () => (
+//   <Button onClick={action('clicked')}>
+//     <span role="img" aria-label="so cool">
+//       😀 😎 👍 💯
+//     </span>
+//   </Button>
+// );
