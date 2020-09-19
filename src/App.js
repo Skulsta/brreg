afa980c1a6ede5ee1ce9b6bb6036037616b4b18a
skulsta <sti.sku@gmail.com>
2020-09-19 16:30:24 +0200

    Removed broken api call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
+import { ThemeProvider, Link } from "@material-ui/core";
+import { theme, Text } from "@tipio/tipio-ui/dist/index";
 import CompanyTable from "./components/CompanyTable/CompanyTable";
 import SearchForm from "./components/SearchForm/SearchForm";
 import "./App.scss";
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const [companyData, setCompanyData] = useState({ enheter: [] });
+    const [open, setOpen] = useState(false);
+    const [companyData, setCompanyData] = useState({ enheter: [] });
 
-  return (
-    <div className="Layout">
-      <SearchForm
-        setOpen={(result) => setOpen(result)}
-        setCompanyData={(result) => setCompanyData(result)}
-      />
-      <CompanyTable open={open} companies={companyData} />
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="Layout">
+                <SearchForm
+                    setOpen={result => setOpen(result)}
+                    setCompanyData={result => setCompanyData(result)}
+                />
+                <CompanyTable open={open} companies={companyData} />
+            </div>
+        </ThemeProvider>
+    );
 };
 
 export default App;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,52 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Button, Form, Container } from "react-bootstrap";
-import axios from "axios";
 import classes from "./SearchForm.module.scss";
 
 const SearchForm = (props) => {
-  const [query, setQuery] = useState("");
-  const [url, setUrl] = useState("");
+    const [query, setQuery] = useState("");
+    const [url, setUrl] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios({ url });
-      const companies = result.data._embedded;
-      props.setCompanyData(companies);
+    //TODO fikse base url
+    const getData = () => {
+        setUrl(
+            `https://data.brreg.no/enhetsregisteret/api/enheter?navn=${query}`
+        );
     };
 
-    fetchData();
-  }, [props, url]);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.setOpen(true);
+        if (query) getData();
+    };
 
-  //TODO fikse base url
-  const getData = () => {
-    setUrl(`https://data.brreg.no/enhetsregisteret/api/enheter?navn=${query}`);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.setOpen(true);
-    if (query) getData();
-  };
-
-  return (
-    <Container className="d-flex row justify-content-center">
-      <Form
-        onSubmit={handleSubmit}
-        className={classes.Form + " d-flex flex-row"}
-      >
-        <Form.Control
-          autoFocus
-          className={classes.SearchField + " mr-2"}
-          placeholder="Bedrift AS"
-          onChange={(event) => setQuery(event.target.value)}
-          size="lg"
-        />
-        <Button type="submit" size="lg">
-          Søk
-        </Button>
-      </Form>
-    </Container>
-  );
+    return (
+        <Container className="d-flex row justify-content-center">
+            <Form
+                onSubmit={handleSubmit}
+                className={classes.Form + " d-flex flex-row"}
+            >
+                <Form.Control
+                    autoFocus
+                    className={classes.SearchField + " mr-2"}
+                    placeholder="Bedrift AS"
+                    onChange={(event) => setQuery(event.target.value)}
+                    size="lg"
+                />
+                <Button type="submit" size="lg">
+                    Søk
+                </Button>
+            </Form>
+        </Container>
+    );
 };
 
 export default SearchForm;
